113f01667ed058d4382f5b7c9871b9304bd0c1ec
Odzhan <odzhan@protonmail.com>
2016-12-07 02:11:22 +0000

    ds

--- crackers/despc/clm5.asm
+++
@@ -1,7 +0,0 @@
-; mark_description "Intel(R) C++ Compiler XE for applications running on IA-32, Version 15.0.0.108 Build 20140726";
-; mark_description "-Fa -O3 -fast -GS-";
-	.686P
- 	.387
-	OPTION DOTNAME
-	ASSUME	CS:FLAT,DS:FLAT,SS:FLAT
-	END
